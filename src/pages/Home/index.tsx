c3885c0c5353243618541701647fddc8527baa2a
Jao8 <joaovictorocosta@gmail.com>
2023-01-16 04:16:03 -0300

    - Image upload created - geolocation plugin added

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,18 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../../components/ExploreContainer';
 import './styles.css';
 import { Camera, CameraResultType } from '@capacitor/camera';
+import { useState } from 'react';
 
-interface imageElement{
+interface imageElement {
   src: string;
 }
 
 const Home: React.FC = () => {
-  
+
+  const [image, setImage] = useState<any>(null)
+  const api = 'http://localhost/sete-entregas/api/images';
+
   const takePicture = async () => {
     console.log('Taking picture');
     const image = await Camera.getPhoto({
@@ -17,17 +21,57 @@ const Home: React.FC = () => {
       resultType: CameraResultType.Uri
     });
     var imageUrl = image.webPath;
-    var imageElement:imageElement = {src:'' };
-    console.log('imageElement', imageElement);
-    console.log('image', image);
-
+    var imageElement: imageElement = { src: '' };
+    setImage(image);
     // Can be set to the src of an image now
     imageElement.src = imageUrl!;
+    uploadImage(imageUrl);
   };
 
+  const base64FromPath = async function(path: string): Promise<string> {
+    const response = await fetch(path);
+    const blob = await response.blob();
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        if (typeof reader.result === 'string') {
+          resolve(reader.result);
+        } else {
+          reject('method did not return a string');
+        }
+      };
+      reader.readAsDataURL(blob);
+    });
+  }
+
+  const uploadImage = async (path: any) => {
+    const base64Image = await base64FromPath(path!);
+    console.log(base64Image);
+    // post data to server
+    const res = await fetch(`${api}/create.php`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        photo: base64Image,
+        location: '123,123'
+      })
+    });
+  }
 
   return (
-    <button onClick={takePicture}>Camera</button>
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Home</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen className='btn-container'>
+        <IonButton onClick={takePicture}>Take Picture</IonButton>
+      </IonContent>
+    </IonPage>
   );
 };
 
